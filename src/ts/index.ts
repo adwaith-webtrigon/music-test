63364d5d6b1ec5a31147e1d12ee45bc001dc1924
Sathvik Patil <sathvikpatil67@gmail.com>
2021-01-30 18:10:00 +0530

    HTML tags

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -10,6 +10,11 @@ function playMusic() {
     // There are 60,000 milliseconds in a minute. So 60,000 / BPM(240) = one beat (250ms)
     let i = 0
     //Check console logs of a browser to see a note playing
+    const Accidental = {
+        'S': "Sharp",
+        'F': "Flat",
+        'N': "Natural"
+    }
     function playNote() {
         if (i >= notes.length) {
             //stop after all notes are completed
@@ -17,11 +22,14 @@ function playMusic() {
         }
 
         if (notes[i].accidental) {
-            let sound = document.getElementById(notes[i].pitch + notes[i].octave + notes[i].accidental)!; (sound as HTMLAudioElement).play();
+            let accidental = Accidental[notes[i].accidental!]
+            let sound = new Audio();
+            sound.src = './src/assets/' + notes[i].pitch + String(notes[i].octave) + " " + accidental + ".mp3";
+            sound.play()
             let currentBeat = oneBeat * notes[i].beats;
             setTimeout(function () {
-                (sound as HTMLAudioElement).pause();
-                (sound as HTMLAudioElement).currentTime = 0;
+                sound.pause();
+                sound.currentTime = 0;
             }, currentBeat);
             console.log(notes[i].pitch + notes[i].octave + notes[i].accidental + " beats " + notes[i].beats + "   " + currentBeat + 'ms')
             i++
@@ -29,11 +37,13 @@ function playMusic() {
 
 
         } else {
-            let sound = document.getElementById(notes[i].pitch + notes[i].octave)!; (sound as HTMLAudioElement).play();
+            let sound = new Audio();
+            sound.src = './src/assets/' + notes[i].pitch + String(notes[i].octave) + ".mp3";
+            sound.play()
             let currentBeat = oneBeat * notes[i].beats;
             setTimeout(function () {
-                (sound as HTMLAudioElement).pause();
-                (sound as HTMLAudioElement).currentTime = 0;
+                sound.pause();
+                sound.currentTime = 0;
             }, currentBeat);
             console.log(notes[i].pitch + notes[i].octave + " beats " + notes[i].beats + "   " + currentBeat + 'ms')
             i++
@@ -45,5 +55,4 @@ function playMusic() {
     playNote(); //calling a recursive function
 
 }
-
 document.getElementById('start-playing')?.addEventListener('click', playMusic);
